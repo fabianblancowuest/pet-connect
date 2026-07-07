4c809fe7846852c7e5dcdc2a0e687d9987feb71b
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2026-07-07 01:56:00 -0300

    Cambiar botón modo claro/oscuro a CSS puro y JS nativo sin Alpine

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,21 +29,21 @@
                     </flux:button>
                     <button
                         type="button"
+                        id="theme-toggle"
                         class="flex size-9 items-center justify-center rounded-lg text-zinc-500 transition-colors hover:bg-zinc-100 hover:text-zinc-700 dark:text-zinc-400 dark:hover:bg-zinc-800 dark:hover:text-zinc-200"
-                        x-data="{ dark: document.documentElement.classList.contains('dark') }"
-                        x-on:click="
-                            dark = !dark;
-                            localStorage.setItem('flux.appearance', dark ? 'dark' : 'light');
+                        onclick="
                             document.documentElement.classList.toggle('dark');
+                            localStorage.setItem('flux.appearance', document.documentElement.classList.contains('dark') ? 'dark' : 'light');
+                            this.setAttribute('title', document.documentElement.classList.contains('dark') ? '{{ __('Modo claro') }}' : '{{ __('Modo oscuro') }}');
                         "
-                        :title="dark ? 'Modo claro' : 'Modo oscuro'"
+                        title="{{ __('Modo oscuro') }}"
                     >
-                        <template x-if="dark">
-                            <flux:icon name="sun" class="size-5" />
-                        </template>
-                        <template x-if="!dark">
-                            <flux:icon name="moon" class="size-5" />
-                        </template>
+                        <svg class="size-5 block dark:hidden" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                        </svg>
+                        <svg class="size-5 hidden dark:block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+                        </svg>
                     </button>
 
                     @auth
